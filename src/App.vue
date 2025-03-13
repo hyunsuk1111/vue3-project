0fbb6bf0fbbd1625f92e344710ab79021eb0b8ca
hyunsuk1111 <epsxkf@gmail.com>
2025-03-13 22:25:11 +0900

    FEAT : v-show 적용
    v-show 적용

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
   <div class="container">
     <div>one more step</div>
     <h2>To-Do List</h2>
-    <form class="d-flex" @submit.prevent="onSubmit">
-      <div class="flex-grow-1 mr-2">
-        <input class="form-control" type="text" placeholder="Type new Todo" v-model="todo">
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input class="form-control" type="text" placeholder="Type new Todo" v-model="todo">
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">Add</button>
+        </div>
       </div>
-      <div>
-        <button class="btn btn-primary" type="submit">Add</button>
-      </div>
+      <div v-show="hasError" style="color: red;">This field cannot be empty</div>
     </form>
     <ul class="card mt-2">
       <li class="card-body p-2" v-for="(item, index) in todoList" :key="index">{{ index + 1 }}, {{ item }}</li>
@@ -24,18 +27,26 @@ import { reactive } from 'vue';
     setup() {
         const todo = ref('');
         const todoList = reactive([]);
+        const hasError = ref(false);
 
         const onSubmit = () => {
-          todoList.push({
+          if(todo.value === '') {
+            hasError.value = true;
+          } else {
+            todoList.push({
               id : Date.now(),
               subject : todo.value,
             });
+
+            hasError.value = false;
+          }
         };
 
       return {
         todo,
-        onSubmit,
         todoList,
+        hasError,
+        onSubmit,
       }
     }
   }
