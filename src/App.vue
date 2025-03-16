1c26e27b0bf7bf731277bf37c7b23026dd348175
hyunsuk1111 <epsxkf@gmail.com>
2025-03-16 17:25:25 +0900

    FEAT : 컴포넌트 적용
    자식 컴포넌트 생성 및 연결

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,13 @@
   <div class="container">
     <div>one more step</div>
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit">
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input class="form-control" type="text" placeholder="Type new Todo" v-model="todo">
-        </div>
-        <div>
-          <button class="btn btn-primary" type="submit">Add</button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color: red;">This field cannot be empty</div>
-    </form>
+    <TodoSimpleForm @add-todoList="addTodolist"/>
+
     <div v-if="todoList.length == 0">Empty todoList</div>
     <div class="card mt-2" v-for="(item, index) in todoList" :key="index">
       <div class="card-body p-2 d-flex align-items-center">
         <div class="form-check flex-grow-1">
           <input class="form-check-input" type="checkbox" v-model="item.completed">
-          <!-- <label class="form-check-lable" :style="item.completed ? todoStyle : {}">{{ index + 1 }}, {{ item.subject }}</label> -->
           <label class="form-check-lable" :class="{ todo : item.completed }">{{ index + 1 }}, {{ item.subject }}</label>
         </div>
         <div><button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button></div>
@@ -28,33 +18,18 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import { reactive } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
   export default {
+    components: {
+      TodoSimpleForm
+    },
     setup() {
-        const todo = ref('');
         const todoList = reactive([]);
-        const hasError = ref(false);
-        /* const todoStyle = {
-          textDecoration: 'line-through',
-          color: 'gray',
-
-        } */
-
-        const onSubmit = () => {
-          if(todo.value === '') {
-            hasError.value = true;
-          } else {
-            todoList.push({
-              id: Date.now(),
-              subject: todo.value,
-              completed: false,
-            });
-
-            hasError.value = false;
-          }
-          todo.value = '';
+        
+        const addTodolist = (todo) => {
+          todoList.push(todo);
         };
 
         const deleteTodo = (index) => {
@@ -62,11 +37,8 @@ import { reactive } from 'vue';
         };
 
       return {
-        todo,
         todoList,
-        hasError,
-        /* todoStyle, */
-        onSubmit,
+        addTodolist,
         deleteTodo,
       }
     }
