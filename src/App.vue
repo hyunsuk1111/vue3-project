fc8ee6dea5385407e113f4ec34027eb280d5ad13
hyunsuk1111 <epsxkf@gmail.com>
2025-03-17 10:30:26 +0900

    FEAT : props 적용
    props 적용

--- src/App.vue
+++ src/App.vue
@@ -3,43 +3,43 @@
     <div>one more step</div>
     <h2>To-Do List</h2>
     <TodoSimpleForm @add-todoList="addTodolist"/>
-
     <div v-if="todoList.length == 0">Empty todoList</div>
-    <div class="card mt-2" v-for="(item, index) in todoList" :key="index">
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input class="form-check-input" type="checkbox" v-model="item.completed">
-          <label class="form-check-lable" :class="{ todo : item.completed }">{{ index + 1 }}, {{ item.subject }}</label>
-        </div>
-        <div><button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button></div>
-      </div>
-    </div>
+    <TodoList :todoList="todoList" @toggle-todo="toggleTodo"/>
   </div>
 </template>
 
 <script>
 import { reactive } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 
   export default {
     components: {
-      TodoSimpleForm
+      TodoSimpleForm,
+      TodoList,
     },
+
     setup() {
         const todoList = reactive([]);
         
         const addTodolist = (todo) => {
           todoList.push(todo);
         };
+        
+        const toggleTodo = (index) => {
+          todoList[index].completed = !todoList[index].completed;
+        };
 
         const deleteTodo = (index) => {
           todoList.splice(index, 1);
         };
 
+
       return {
         todoList,
         addTodolist,
         deleteTodo,
+        toggleTodo,
       }
     }
   }
